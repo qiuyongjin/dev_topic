f00650a3443668aa3c2db1dcd538201c9ee8ad44
yqiu <qiuyj@lansee.net>
2018-04-28 13:51:32 +0800

    “enhance”

---
+++ src/page/model/mobile.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="mobile-model">
+        mobile model
+    </div>
+</template>
+
+<script>
+    // import {getProject, getUserInfo, addProject} from "../../../assets/js/getData";
+    // import {ding} from '../../../assets/js/common'
+
+    export default {
+        name: 'dev-topic',
+        data () {
+            return {
+                status: 0, // 0:mobile 1:PC
+            }
+        },
+        created () {
+
+        },
+        methods: {
+            funMP () {
+                this.status = (this.status) ? 0 : 1;
+            }
+        },
+        components: {}
+    }
+</script>
+
+<style scoped lang="scss">
+    @import "../../assets/style/mixin";
+
+    #mobile-model {
+        width: 375px;
+        height: 667px;
+        color: #000;
+        margin-left: 10%;
+        background: #fff;
+    }
+</style>
